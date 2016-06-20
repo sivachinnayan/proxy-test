8c80ba2061fcff30f50f1de650607680a0ca0f48
sivachinnayan <sivasubramaniyam.chinnayan@kore.com>
2016-06-20 23:45:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-//var request = require('request');
-//var http = require('http');
-//var debug = require('debug');
-//
-//var r = request.defaults({'proxy':'http://localhost:3128'})
-//
-//http.createServer(function (req, resp) {
-//  console.log(req);
-//  debug(req);
-//  if (req.url === '/doodle.png') {
-//    r.get('http://google.com/doodle.png').pipe(resp)
-//  }
-//});
 'use strict';
 var debug = require('debug');
 var http = require('http');
